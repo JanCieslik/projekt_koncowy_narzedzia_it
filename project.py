2690ad42f39a6cd0bcd2088e5e8be70d104a9490
JanCieslik <cieslikjan8@gmail.com>
2024-05-27 13:13:25 +0200

    dodano instrukcje warunkowe do obslugi funkcji

--- project.py
+++ project.py
@@ -22,12 +22,12 @@ def zapisywanie_json(file_path, wczytane):
     with open(file_path, 'w') as file:
         json.dump(wczytane, file, indent=4)
 def wczytywanie_yml(file_path):
-    with open(file_path, 'r') as file_yml:
-        wczytane_yml = yaml.safe_load(file_yml)
-    return wczytane_yml
-def zapisywanie_yml(file_path, wczytane_yml):
-    with open(file_path, 'w') as file_yml:
-        yaml.dump(wczytane_yml, file_yml, default_flow_style=False)
+    with open(file_path, 'r') as file:
+        wczytane = yaml.safe_load(file)
+    return wczytane
+def zapisywanie_yml(file_path, wczytane):
+    with open(file_path, 'w') as file:
+        yaml.dump(wczytane, file, default_flow_style=False)
 def wczytywanie_xml(file_path):
     tree = et.parse(file_path)
     root = tree.getroot()
@@ -38,4 +38,24 @@ def zapisywanie_xml(file_path, tree):
 
 if __name__ == "__main__":
     input_file, output_file, input_ext, output_ext = parsowanie()
-    print(f"Plik wejsciowy: {input_file}, Plik wyjsciowy: {output_file}")
+
+    wczytane = None
+
+    if input_ext == '.json':
+        wczytane = wczytywanie_json(input_file)
+    elif input_ext == '.yml' or input_ext == '.yaml':
+        wczytane = wczytywanie_yml(input_file)
+    elif input_ext == '.xml':
+        tree, root = wczytywanie_yml(input_file)
+        wczytane = et.tostring(root, encoding='unicode')
+
+
+    if output_ext == '.json':
+        zapisywanie_json(output_file, wczytane)
+    elif output_ext == '.yml' or output_file =='.yaml':
+        zapisywanie_yml(output_file, wczytane)
+    elif output_ext == '.xml':
+        if isinstance(wczytane, str):
+            root = et.fromstring(wczytane)
+            tree = et.ElementTree(root)
+        zapisywanie_xml(output_file, tree)
